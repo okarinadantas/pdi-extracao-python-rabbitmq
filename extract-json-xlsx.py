a855f86398bdd65b6d0f061bb3b542bec9e2d031
karina <karinadantasluiz@gmail.com>
2023-02-02 18:24:04 -0300

    update-comments

--- extract-json-xlsx.py
+++ extract-json-xlsx.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+# Read json file
 data = pd.read_json("queues-binds.json")
+
+# Group data by queue and binds
 grouped_data = data.groupby(by=["destination", "source"]).sum()
 
+# Export as .xlsx
 grouped_data.to_excel("queues-binds.xlsx", engine="openpyxl")
